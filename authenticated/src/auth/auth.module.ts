8477b52206615f401fb011e083962ff719b59ffe
Vitonel0404 <victor0404.vmhm@gmail.com>
2025-05-29 13:24:19 -0500

    fixed user import auth 2

--- authenticated/src/auth/auth.module.ts
+++ authenticated/src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminUser } from './entities/admin-user.entity';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([AdminUser, User])],
   controllers: [AuthController],
   providers: [AuthService],
 })
